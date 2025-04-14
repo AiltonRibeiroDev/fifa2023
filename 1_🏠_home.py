2208a349ffdb27114a307e796435cfe12e435241
Ailton Ribeiro <ribeiroailtonalves@gmail.com>
2025-04-13 21:53:10 -0300

    Adicionando projeto

---
+++ 1_🏠_home.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import webbrowser
+import pandas as pd
+from datetime import datetime
+
+
+if "data" not in st.session_state:
+    df_data = pd.read_csv("K:/curso_streamlit/datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
+    df_data = df_data[df_data["Value(£)"] > 0]
+    df_data = df_data.sort_values(by="Overall", ascending=False)
+    st.session_state["data"] = df_data
+
+st.markdown("# FIFA23 OFFICIAL SATASET! ⚽")
+st.sidebar.markdown("Desenvolvido por [Ailton Ribeiro](https://github.com/AiltonRibeiroDev)")
+
+btn = st.button("Acesse os dados no Kaglle")
+if btn:
+    webbrowser.open_new_tab("https://www.kaggle.com/datasets/bryanb/fifa-player-stats-database")
+
+st.markdown("""
+
+O conjunto de dados de jogadores de futebol de 2017 a 2023 fornece informações abrangentes sobre  jogadores de futebol profissionais. O conjunto de dados contém uma ampla gama de atributos, incluindo  dados demográficos do jogador, características físicas, estatísticas de jogo, detalhes do contrato e  afiliações de clubes.  
+Com mais de 17.000 registros, este conjunto de dados oferece um recurso valioso para analistas de  futebol, pesquisadores e entusiastas interessados em explorar vários aspectos do mundo do futebol, pais  permite estudar atributos de jogadores, métricas de desempenho, avaliação de mercado, analise de  clubes, 
+posicionamento de jogadores e desenvolvimento do jogador ao longo do tempo.
+
+""")
